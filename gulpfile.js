e9ec719d7c6f54a5714984570c97a5aa3acc5138
pip_lineupbuilder <howley.phil@gmail.com>
2017-01-24 12:34:59 -0800

    remove demo task and add paths object

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
-
-gulp.task('demo', function() {
-  console.log('Demo working');
-});
+    connect = require('gulp-connect'),
+    paths = {
+      js: {
+        all: ['./src/js/global.js',
+             './src/js/about.js',
+             './src/js/contact.js',
+             './src/js/index.js',
+             './src/js/live.js',
+             './src/js/photos.js']
+      }
+    };
 
 gulp.task('connect', function() {
   connect.server();
 });
 
-gulp.task('default', ['connect', 'demo']);
+gulp.task('default', ['connect']);
